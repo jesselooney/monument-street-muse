c8f50592237db9e4f993686dd9be3ab31f27a2fe
Impartiall <42242696+Impartiall@users.noreply.github.com>
2022-06-12 15:06:59 -0400

    Allow other names for localhost as hosts

--- monument_street_muse/settings.py
+++ monument_street_muse/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'monument-street-muse.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'monument-street-muse.herokuapp.com']
 
 
 # Application definition
