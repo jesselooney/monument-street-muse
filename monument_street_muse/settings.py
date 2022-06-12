6a1fadde66c92dec9d747fd72c35edee2c3b4b4a
Impartiall <42242696+Impartiall@users.noreply.github.com>
2022-06-12 14:41:40 -0400

    Enable whitenoise for static files

--- monument_street_muse/settings.py
+++ monument_street_muse/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,7 +130,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'monument_street_muse/static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'    
+STATIC_URL = '/static/'
+
+# Whitenoise gzip functionality
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
